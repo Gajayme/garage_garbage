a705a259f2062cb72a55e4036b5b7a86929da861
Георгий Шахов <g.shakhov@wisetech.pro>
2025-01-28 20:47:57 +0100

    Added deletable picture

---
+++ src/UploadPage/ImageManager/DeletableImage.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+import "../../Styles/ImageManager/DeletableImage.css"
+import DeleteIcon from "../../Imgs/delete_image_cross.png"
+
+
+export const DeletableImage = ({ image, onDeleteSpecific }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <div
+            className="deletable-image-container"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
+
+            <img key={image.id} className="deletable-image" src={image.src} alt={image.alt || `Image ${image.id}`}/>
+
+            {isHovered && (
+                <img className="delete-icon" src={DeleteIcon} alt="Delete icon" onClick={() => onDeleteSpecific(image.id)}/>
+            )}
+        </div>
+    )
+};
--- src/UploadPage/ImageManager/ImageViewer.js
+++ src/UploadPage/ImageManager/ImageViewer.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import {DeletableImage} from "./DeletableImage";
 import '../../Styles/ImageManager/ImageViewer.css'
 
 export const ImageViewer = ({ images, onDeleteSpecific }) => {
     return (
         <div className='image-container'>
-			{images.length === 0 ? (
+            {images.length === 0 ? (
                 <p>No uploaded images.</p>
             ) : (
                 images.map((image, _) => (
-                	<img key={image.id} className="image-item" src={image.src} alt={image.alt || `Image ${image.id}`} onClick={() => onDeleteSpecific(image.id)} />
+                    <DeletableImage image={image} onDeleteSpecific={onDeleteSpecific}/>
+                    // <img key={image.id} className="image-item" src={image.src} alt={image.alt || `Image ${image.id}`} onClick={() => onDeleteSpecific(image.id)} />
                 ))
             )}
         </div>
