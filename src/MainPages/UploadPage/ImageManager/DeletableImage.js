f2a3711862db8955e6a9a2f46a3d2c5d00d0513e
Георгий Шахов <g.shakhov@wisetech.pro>
2025-07-21 14:57:02 +0400

    changed delete image cross, delete image cross is shown always

--- src/MainPages/UploadPage/ImageManager/DeletableImage.js
+++ src/MainPages/UploadPage/ImageManager/DeletableImage.js
@@ -1,22 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import "../../../Styles/ImageManager/DeletableImage.css"
 import DeleteIcon from "../../../Imgs/delete_image_cross.png"
 
 
-export const DeletableImage = ({ image, onDeleteSpecific }) => {
-    const [isHovered, setIsHovered] = useState(false);
+export const DeletableImage = ({ image, onDeleteSpecific}) => {
     return (
-        <div
-            className="deletable-image-container"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
-
+        <div className="deletable-image-container">
             <img key={image.id} className="deletable-image" src={image.src} alt={image.alt || `Image ${image.id}`}/>
-
-            {isHovered && (
-                <img className="delete-icon" src={DeleteIcon} alt="Delete icon" onClick={() => onDeleteSpecific(image.id)}/>
-            )}
+            <img className="delete-icon" src={DeleteIcon} alt="Delete icon" onClick={() => onDeleteSpecific(image.id)}/>
         </div>
     )
 };
