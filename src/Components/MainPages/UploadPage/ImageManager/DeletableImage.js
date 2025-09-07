0ac51cc831f8f0fd1777b9e6aeb82dab20efdb5b
Георгий Шахов <g.shakhov@wisetech.pro>
2025-09-07 15:43:23 +0200

    added bordered image style

--- src/Components/MainPages/UploadPage/ImageManager/DeletableImage.js
+++ src/Components/MainPages/UploadPage/ImageManager/DeletableImage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import "Styles/MainPages/UploadPage/ImageManager/DeletableImage.css"
+import "Styles/BorderedImage.css"
 import DeleteIcon from "Images/delete_image_cross.png"
 
 
 export const DeletableImage = ({ image, onDeleteSpecific}) => {
     return (
         <div className="deletable-image-container">
-            <img key={image.id} className="deletable-image" src={image.src} alt={image.alt || `Image ${image.id}`}/>
+            <img key={image.id} className="deletable-image bordered-image" src={image.src} alt={image.alt || `Image ${image.id}`}/>
             <img className="delete-icon" src={DeleteIcon} alt="Delete icon" onClick={() => onDeleteSpecific(image.id)}/>
         </div>
     )
