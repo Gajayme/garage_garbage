1172b9b27793d18e127080aa82bc34b662ba3c26
Георгий Шахов <g.shakhov@wisetech.pro>
2025-09-20 15:18:09 +0200

    removed all code from database page

--- src/Components/MainPages/DatabasePage/DatabasePageContent.js
+++ src/Components/MainPages/DatabasePage/DatabasePageContent.js
@@ -8,103 +8,49 @@ import "Styles/MainPages/DatabasePage/DatabaseItems.css"
 import "Styles/MainPages/DatabasePage/DatabasePage.css"
 import "Styles/MainPages/DatabasePage/FiltersActivationButton.css"
 
-import DefaultImg from "Images/сложенная-пустая-одежда-на-белом-фоне-329728970.webp"
-
-import {click} from "@testing-library/user-event/dist/click";
 
 export const DatabasePageContent = () => {
 
 	const onFilterClicked = () => {
+
 	}
 
-	    //
-    // // стейт для сохранения полученных с сервера объектов
-    // const [databaseState, setDatabaseState] = useState([])
-    // const [chosenItem, setChosenItem] = useState(null)
-    //
-    // const parseServerData = (data) => {
-    //     console.log(data)
-    //     const newItems = data.map((item) => ({
-    //         id: item.id, // TODO: переделать на нормальное значение
-    //         image:  Array.isArray(item.images) && item.images.length && item.images[0].image_url, // TODO: переделать на нормальное значение
-    //         itemName: item.itemName,
-    //         price: item.price,
-    //         adding_date_time: 0, // TODO: переделать на нормальное значение
-    //     }));
-    //     setDatabaseState((prevState) => [...prevState, ...newItems]);
-    // }
-    //
+    // стейт для сохранения полученных с сервера объектов
+    const [databaseState, setDatabaseState] = useState([])
+
+    const parseServerData = (data) => {
+        console.log(data)
+        const newItems = data.map((item) => ({
+            id: item.id, // TODO: переделать на нормальное значение
+            image:  Array.isArray(item.images) && item.images.length && item.images[0].image_url, // TODO: переделать на нормальное значение
+            itemName: item.itemName,
+            price: item.price,
+            adding_date_time: 0, // TODO: переделать на нормальное значение
+        }));
+        setDatabaseState((prevState) => [...prevState, ...newItems]);
+    }
+
+	// Запрос к серверу
+    useEffect(() => {
+        if (!databaseState.length) {
+			fetch(Constants.base_server_url + Constants.post_all, {
+                method: Constants.http_methods.GET,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => parseServerData(data.data))
+                .catch((error) => console.error('Ошибка:', error));
+
+			}
+    }, [databaseState.length]);
 
 
     return (
         <div className="database-page">
             <DefaultButton className={"filter-activation-button"} labelText={"Filters"} onClick={onFilterClicked}></DefaultButton>
-			<Items/>
+			<Items databaseState/>
         </div>
     )
-
-
-    // // Запрос к серверу
-    // useEffect(() => {
-    //     if (!databaseState.length) {
-    //         fetch(Constants.base_server_url + Constants.post_all, {
-    //             method: Constants.http_methods.GET,
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //         })
-    //             .then((response) => response.json())
-    //             .then((data) => parseServerData(data.data))
-    //             .catch((error) => console.error('Ошибка:', error));
-    //     }
-    // }, [databaseState.length]);
-    //
-    // useEffect(() => {
-    //
-    //     // Функция для обработки клика вне item
-    //     const handleOutsideClick = (event) => {
-    //         // Проверяем, был ли клик внутри модального окна
-    //         if (chosenItem && !event.target.closest(".modal-overlay") && !event.target.closest(".database-item")) {
-    //             setChosenItem(null);
-    //         }
-    //     };
-    //
-    //     document.addEventListener("click", handleOutsideClick);
-    //     return () => {
-    //         document.removeEventListener("click", handleOutsideClick);
-    //     };
-    // }, [chosenItem]);
-    //
-    // const handleItemClick = (item) => {
-    //     if (chosenItem) {
-    //         setChosenItem(null);
-    //     } else {
-    //         setChosenItem(item);
-    //     }
-    // }
-    //
-    // return (
-    //     <div>
-    //
-    //         {chosenItem && (
-    //             <ModalWindow
-    //                 className={"modal-overlay"}
-    //                 itemId={chosenItem.id}/>
-    //         )}
-    //
-    //         <div className={"database-items"}>
-    //             {databaseState.map((item) => (
-    //                 <Item
-    //                     key={item.id}
-    //                     img={item.image}
-    //                     name={item.itemName}
-    //                     price={item.price}
-    //                     adding_date_time={item.adding_date_time}
-    //                     onClick={() => handleItemClick(item)
-    //                     }
-    //                 />
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
 }
--- src/Components/MainPages/DatabasePage/Items.js
+++ src/Components/MainPages/DatabasePage/Items.js
@@ -3,13 +3,14 @@ import {Item} from 'Components/MainPages/DatabasePage/Item';
 
 import DefaultImg from "Images/сложенная-пустая-одежда-на-белом-фоне-329728970.webp"
 
-export const Items = () => {
+export const Items = ({databaseState}) => {
 
 	const onClick = () => {
         console.log("clicked");
     }
 
 	return (
+
 		<div className="database-items">
 			<Item img={DefaultImg} name={"Super Item"} price={"999"} onClick={onClick}/>
 			<Item img={DefaultImg} name={"Super Item"} price={"999"} onClick={onClick}/>
