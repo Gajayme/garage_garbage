8e984d2e502476f0ff62343c819b2343dd7d712f
Георгий Шахов <g.shakhov@wisetech.pro>
2025-12-16 21:13:23 +0100

    reworked NavButton with NavLink, made custom styles for the selected NavLink

--- src/Components/Navigation/NavButton.js
+++ src/Components/Navigation/NavButton.js
@@ -1,17 +1,12 @@
-import {useNavigate} from 'react-router-dom';
-import {DefaultButton} from "Components/Button";
+import { NavLink } from "react-router-dom";
 
+export const NavButton = ({ labelText, destination }) => {
+	return (
+		<NavLink
+			to={destination}
+			className={({ isActive }) => isActive ? "nav-button nav-button--selected" : "nav-button"} end >
 
-/**
- * Навигационная кнопка.
- *
- * @param {string} className - Имя класса для стилей.
- * @param {string} labelText - Заголовок кнопки.
- * @param {string} destination - Дестинация, куда будет перенаправлен пользователь после нажатия кнопки.
- */
-export const NavButton = ({className, labelText, destination}) => {
-    const navigate = useNavigate();
-    return (
-        <DefaultButton className={className} labelText={labelText} onClick={() => navigate(destination)}/>
-    )
-}
+			{labelText}
+		</NavLink>
+	);
+};
