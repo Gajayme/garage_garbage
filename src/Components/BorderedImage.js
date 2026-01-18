5647725be4f3c7b02e4e8a463b150b6a9e53fbc6
Георгий Шахов <g.shakhov@wisetech.pro>
2026-01-18 12:52:39 +0100

    fixed small issues, deleted redundant files

--- src/Components/BorderedImage.js
+++ src/Components/BorderedImage.js
@@ -3,14 +3,13 @@ import "Styles/TopAndLeftBorders.css"
 import DefaultImg from "Images/default.jpg"
 
 
-export const BorderedImage = ({className, imageSrc, altText, key = 0}) => {
+export const BorderedImage = ({className, imageSrc, altText}) => {
 
 	return (
 		<img
-			className={`top-and-left-borders + ${className}`}
+			className={`top-and-left-borders ${className ?? ""}`}
 			src={imageSrc ? imageSrc : DefaultImg}
 			alt={altText}
-			key={key}
 		/>
 	)
 }
