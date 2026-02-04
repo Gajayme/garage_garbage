ba0dc42755d8ee6564a25bc17e8270dbf6f748e1
Георгий Шахов <g.shakhov@wisetech.pro>
2026-02-04 19:30:04 +0100

    cleaned styles for navigation buttons, image manager

--- src/Components/MainPages/UploadPage/ImageManager/DeletableImage.js
+++ src/Components/MainPages/UploadPage/ImageManager/DeletableImage.js
@@ -9,7 +9,7 @@ import DeleteIcon from "Images/delete_image_cross.png"
 export const DeletableImage = ({ image, onDeleteSpecific}) => {
 	return (
 		<div className="deletable-image-container top-and-left-borders">
-			<img className="no-baseline-img deletable-image" key={image.id}  src={image.src} alt={image.alt || `Image ${image.id}`}/>
+			<img className="no-baseline-img deletable-image" src={image.src} alt={image.alt || `Image ${image.id}`}/>
 			<img className="delete-icon" src={DeleteIcon} alt="Delete icon" onClick={() => onDeleteSpecific(image.id)}/>
 		</div>
 	)
