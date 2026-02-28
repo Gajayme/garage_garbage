cd55091292c0cd7bd62470020fd0e29e69328d40
Георгий Шахов <g.shakhov@wisetech.pro>
2026-02-28 10:33:57 +0100

    changed nav-icon images, added custom favicon

--- src/Components/MainPages/MainPage/Icons.js
+++ src/Components/MainPages/MainPage/Icons.js
@@ -1,13 +1,14 @@
 import {NavIcon} from "Components/Navigation/NavIcon";
 import * as Constants from "Components/Navigation/Constants";
 
-import catalogLogo from "Images/NavIcons/catalog_logo.png"
+import catalogLogo from "Images/NavIcons/trash_bin.png"
+import mainLogo from "Images/NavIcons/computer.png"
 
 
 export const NavIcons = ({className}) => {
     return (
         <div className={className}>
-            <NavIcon image = {catalogLogo} labelText={Constants.rootLabel} destination={Constants.root}/>
+            <NavIcon image = {mainLogo} labelText={Constants.rootLabel} destination={Constants.root}/>
             <NavIcon image = {catalogLogo} labelText={Constants.uploadLabel} destination={Constants.root + Constants.upload}/>
             <NavIcon image = {catalogLogo} labelText={Constants.catalogLabel} destination={Constants.root + Constants.catalog}/>
         </div>
