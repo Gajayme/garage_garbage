157d75bc35909c2929e2aa445d15b9bedab9c4c6
Георгий Шахов <g.shakhov@wisetech.pro>
2026-05-09 12:59:39 +0200

    input params can be invalidated only manually

--- src/Components/hooks/useInputParams.js
+++ src/Components/hooks/useInputParams.js
@@ -6,26 +6,36 @@ export const useInputParams = () => {
 	const { data: brands, isLoading: brandsLoading } = useQuery({
 		queryKey: [GlobalConstants.brandsQueryKey],
 		queryFn: () => fetchInputData(GlobalConstants.brandApi),
+		staleTime: Infinity, // данные не будут дергаться с сервера при прошествии таймера
+		gcTime: Infinity,   // данные не будут удаляться из кеша при прошествии таймера
 	});
 
 	const { data: types, isLoading: typesLoading } = useQuery({
 		queryKey: [GlobalConstants.typesQueryKey],
 		queryFn: () => fetchInputData(GlobalConstants.typeApi),
+		staleTime: Infinity,
+		gcTime: Infinity,
 	});
 
 	const { data: buyers, isLoading: buyersLoading } = useQuery({
 		queryKey: [GlobalConstants.buyersQueryKey],
 		queryFn: () => fetchInputData(GlobalConstants.buyerApi),
+		staleTime: Infinity,
+		gcTime: Infinity,
 	});
 
 	const { data: locations, isLoading: locationsLoading } = useQuery({
 		queryKey: [GlobalConstants.locationsQueryKey],
 		queryFn: () => fetchInputData(GlobalConstants.locationApi),
+		staleTime: Infinity,
+		gcTime: Infinity,
 	});
 
 	const { data: statuses, isLoading: statusesLoading } = useQuery({
 		queryKey: [GlobalConstants.statusesQueryKey],
 		queryFn: () => fetchInputData(GlobalConstants.statusApi),
+		staleTime: Infinity,
+		gcTime: Infinity,
 	});
 
 	return {
